027f294d1d0dd20da21efa8d995b2ed8f6cd5e4d
zolunX10 <zolunx10@hotmail.com>
2012-01-03 19:09:44 +0800

    home.js calculate colStart

--- _/js/home.js
+++ _/js/home.js
@@ -42,11 +42,36 @@ $(document).ready(function()
 	});
 
     var textbox = $('<div>');
-
+    var col={},         //每个tag对应的列数
+        colStart={},    //每个tag的起始列数
+        tagList={       
+            time: [],
+            type: []
+        };
+    function colcount(text,items) {
+        if(text !='Time' && text != 'Type'){
+            var c = items.length;
+            col[text] = Math.floor(c/5.5)+1;
+            if (parseInt(text) >1900) {
+                tagList.time.push(text);
+            } else {
+                tagList.type.push(text);
+            }
+        }
+    }
+    $.each(itemsByTags, colcount);
+    tagList.time.sort();
+    $.each(tagList, function(k, v) {
+        start=0;
+        for (var i=0, ii=v.length; i<ii; i++) {
+            colStart[v[i]]=start;
+            start+=col[v[i]];
+        }
+    });
 	// Looping though the arrays in itemsByTags:
 	$.each(itemsByTags,function(k,v){
 		createList(k,v);
-       	});
+    });
     textbox.appendTo('#imagebox');    
     function showTime(){
           //$("#imagebox li.type").removeClass("vesibilityclass").addClass("unvesibilityclass");
@@ -125,70 +150,79 @@ $(document).ready(function()
         };
                 
     function createList(text,items){
-		
-		// This is a helper function that takes the
-		// text of a menu button and array of li items
-		
-		// Creating an empty unordered list:
+        
+        // This is a helper function that takes the
+        // text of a menu button and array of li items
+        
+        // Creating an empty unordered list:
         if(text != 'Type' && text != 'Time'){
-		var ul = $('<ul>');
+        var ul = $('<ul>');
         var j,k;//k for the opacity
+        /*
         switch(text){
-	case('2002'):
-   	    j=0;
-	    k=1;
-	    break; 				
+    case('2002'):
+        j=0;
+        k=1;
+        break;              
         case('2004'):
-            j=1;
+            j=col['2002'];
             k=1;
             break;
-	case('2006'):
-	    j=2;
+    case('2006'):
+        j=col['2002']+col['2004'];
             k=1;
-	    break;
+        break;
         case('urban'):
             j=0;
             k=0;
             break;
         case('2009'):
-            j=3;
+            j=col['2002']+col['2004']+col['2006'];
             k=1;
             break;
         case('country'):
-            j=1;
+            j=col['urban'];
             k=0;
             break;
         case('building'):
-            j=2;
+            j=col['urban']+col['country'];
             k=0;
             break;
         case('2010'):
-            j=4;
+            j=col['2002']+col['2004']+col['2006']+col['2009'];
             k=1;
             break;
         case('landscape'):
-            j=3;
+            j=col['urban']+col['country']+col['building'];
             k=0;
             break;
        case('2011'):
-            j=5;
+            j=col['2002']+col['2004']+col['2006']+col['2009']+col['2010'];
             k=1;
             break;
         case('other'):
-            j=4;
+            j=col['urban']+col['country']+col['building']+col['landscape'];
             k=0;
             break;
         default:
             j=0;
             k=1;
+        }*/
+        j= colStart[text];
+        if (parseInt(text) >1900) {
+            k=1;
+        } else {
+            k=0;
         }
-	    $.each(items,function(index,value){
-			// Creating a copy of each li item
-			// and adding it to the list:
-	    var element = $(this);
-	   // if(index>4){
-	        element.css({'position':'absolute','bottom':12+(index%5)*72,'left':j*132+66*Math.floor(index/5),'height':'auto'});
-	    //}
+        var c=0;
+        $.each(items,function(index,value){
+            // Creating a copy of each li item
+            // and adding it to the list:
+        var element = $(this);
+        c=c+1;
+       // if(index>4){
+            element.css({'position':'absolute','bottom':12+(index%5)*72,'left':j*70+66*Math.floor(index/5),'height':'auto'});
+        //}
             //else{
               //  element.css({'position':'absolute','bottom':12+index*72,'left':j*132,'height':'auto'});
             //}
@@ -203,12 +237,12 @@ $(document).ready(function()
                 element.removeClass("type").addClass("time");
             }
             element.clone().appendTo(ul);
-		});
+        });
         ul.appendTo('#imagebox');
         var tex = $('<span>',{
                 html:text
         });
-        tex.css({'position':'absolute','bottom':'0px','left':j*132+66});
+        tex.css({'position':'absolute','bottom':'0px','left':j*70+66*(Math.floor(c/5.0001)+1)/2});
         if(k==0){
             tex.removeClass("vesibilityclass").addClass("unvesibilityclass");
             tex.removeClass("time").addClass("type");
@@ -220,14 +254,14 @@ $(document).ready(function()
         tex.appendTo(textbox);
         }
         else{
-		// Creating a menu item. The unordered list is added
-		// as a data parameter (available via .data('list'):
+        // Creating a menu item. The unordered list is added
+        // as a data parameter (available via .data('list'):
         /*
-		var a = $('<a>',{
-			html: text,
-			href:'#',
-			data: {list:ul}
-		}).appendTo('#filter');
+        var a = $('<a>',{
+            html: text,
+            href:'#',
+            data: {list:ul}
+        }).appendTo('#filter');
         */
         }
     }
@@ -252,7 +286,6 @@ $(document).ready(function()
         $('#items').append($filter);
         var isFilter= true;
         $pageNav.hide();
-        console.log($more);
         $more.click(function() {
             if (isFilter) {
                 $filter.hide();
